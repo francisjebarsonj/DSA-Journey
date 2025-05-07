a83fe247c0eb0418e81df9ba8941af76e7eb6413
francisjebarsonj <francisjebarsonj@gmail.com>
2025-05-08 00:29:32 +0530

    Update Patterns_7.java
    Bug Fix on Pattern_7 in Strivers A-Z Patterns

--- src/main/java/com/dsa/code/Striver/Patterns/Patterns_7.java
+++ src/main/java/com/dsa/code/Striver/Patterns/Patterns_7.java
@@ -3,7 +3,10 @@ package com.dsa.code.Striver.Patterns;
 public class Patterns_7 {
     public static void main(String[] args) {
 
-        System.out.println("*");
+        for (int x = 0; x <=5; x++) {
+            System.out.print(" ");
+        }
+        System.out.print("*");
         System.out.println();
         for (int i = 1; i <=3; i++) {
 
