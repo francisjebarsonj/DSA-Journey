4ef2b6934503cc819c7c7ab55cabf5f29d07ba8d
francisjebarsonj <francisjebarsonj@gmail.com>
2025-05-07 23:55:51 +0530

    Logical Thinking Patterns Problems till 7

---
+++ src/main/java/com/dsa/code/Striver/Patterns/Patterns_7.java
@@ -0,0 +1,21 @@
+package com.dsa.code.Striver.Patterns;
+
+public class Patterns_7 {
+    public static void main(String[] args) {
+
+        System.out.println("*");
+        System.out.println();
+        for (int i = 1; i <=3; i++) {
+
+               for (int j = 6 - i; j >= 1; j--) {
+                   System.out.print(" ");
+               }
+
+                for (int j = 1; j <=i*3; j++) {
+                    System.out.print("*");
+                }
+            System.out.println(" ");
+        }
+
+    }
+}
