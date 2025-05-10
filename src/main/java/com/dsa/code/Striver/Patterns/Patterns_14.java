d6e72ff2e0a8f1eb2c37e896740451b8cee5e6ba
francisjebarsonj <francisjebarsonj@gmail.com>
2025-05-10 22:51:47 +0530

    Modified Pattern 14 : Printing char based on N
    Modified Pattern 14 : Printing char based on N

--- src/main/java/com/dsa/code/Striver/Patterns/Patterns_14.java
+++ src/main/java/com/dsa/code/Striver/Patterns/Patterns_14.java
@@ -1,9 +1,12 @@
 package com.dsa.code.Striver.Patterns;
 
+import java.awt.geom.QuadCurve2D;
+import java.util.Scanner;
+
 public class Patterns_14 {
     public static void main(String[] args) {
 
-         /*
+        /*
          * A
          * A B
          * A B C
@@ -11,13 +14,15 @@ public class Patterns_14 {
          * A B C D E
          *
          * */
-        char[] c=new char[]{'A','B','C','D','E','F'};
-
-         for(int i=0; i<c.length; i++){
-             for(int j=0; j<=i; j++){
-                 System.out.print(c[j]);
-             }
-             System.out.println();
-         }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter the value here :");
+        int N = sc.nextInt();
+        char[] c = new char[]{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(c[j]);
+            }
+            System.out.println();
+        }
     }
 }
