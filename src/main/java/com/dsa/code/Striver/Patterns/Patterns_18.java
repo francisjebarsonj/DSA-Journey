99e881aec400197dff9c2bba88a159fcb00c2131
francisjebarsonj <francisjebarsonj@gmail.com>
2025-05-11 19:33:54 +0530

    Solved Pattern Problem 18
    Solved Pattern Problem 18

---
+++ src/main/java/com/dsa/code/Striver/Patterns/Patterns_18.java
@@ -0,0 +1,24 @@
+package com.dsa.code.Striver.Patterns;
+
+public class Patterns_18 {
+    public static void main(String[] args) {
+
+         /*
+        E
+        DE
+        CDE
+        BCDE
+        ABCDE
+         * */
+        char[] c = new char[]{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+        int index = 4;
+        for(int i=0;i<5;i++){
+            for(int j=i;j>=0;j--){
+                // Trick is to find reversal order so we are subtracting with 'j'
+                System.out.print(c[index-j]);
+            }
+            System.out.println();
+        }
+    }
+}
