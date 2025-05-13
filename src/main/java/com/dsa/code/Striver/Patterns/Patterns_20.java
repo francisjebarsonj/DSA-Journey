e08ad37513c601df17f70940f6a06538c545175f
francisjebarsonj <francisjebarsonj@gmail.com>
2025-05-13 09:36:48 +0530

    Solved 20th Pattern Problem
    Solved 20th Pattern Problem

--- src/main/java/com/dsa/code/Striver/Patterns/Patterns_20.java
+++ src/main/java/com/dsa/code/Striver/Patterns/Patterns_20.java
@@ -8,13 +8,32 @@ public class Patterns_20 {
             for(int j=i;j>=0;j--){
                 System.out.print("*");
             }
-            System.out.println();
-        }
-        for(int i=0;i<=5;i++){
-            for(int j=5;j>=i;j--){
+            for(int j=4;j>i;j--){
+                System.out.print(" ");
+            }
+            for(int j=i;j<4;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<=i;j++){
                 System.out.print("*");
             }
             System.out.println();
         }
+        for(int i=1;i<5;i++){
+            for(int j=5;j>i;j--){
+                System.out.print("*");
+            }
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            for(int j=i;j<=4;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
     }
 }
