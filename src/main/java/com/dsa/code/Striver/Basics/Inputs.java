fc150df7e047fbaa72935612f315b7964d0976c0
francisjebarsonj <francisjebarsonj@gmail.com>
2025-05-16 23:25:52 +0530

    Learned Basic User Input/ Output in Java
    Lec 1: Things to Know in C++/Java/Python or any language - User Input / Output

---
+++ src/main/java/com/dsa/code/Striver/Basics/Inputs.java
@@ -0,0 +1,33 @@
+package com.dsa.code.Striver.Basics;
+
+import java.util.Scanner;
+
+public class Inputs {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("The array is: ");
+        for(int i = 0; i < n; i++){
+            System.out.print(arr[i] + " ");
+        }
+
+        // To get Boolean Value
+        System.out.println("Enter Boolean Value:");
+        boolean flag = sc.nextBoolean();
+        System.out.println("Enter Long Value:");
+        // To get Long value
+        long longVal = sc.nextLong();
+        System.out.println("Enter Any Float Value:");
+
+        // To get decimal float values
+        float floatVal = sc.nextFloat();
+
+        System.out.println("Flag:"+flag+" Long Value:"+longVal+" Float Value:"+floatVal);
+
+    }
+}
