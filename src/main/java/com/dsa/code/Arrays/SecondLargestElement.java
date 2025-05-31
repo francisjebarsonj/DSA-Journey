1c36a60abb3122e3642f6592f7d64c69c44d7248
francisjebarsonj <francisjebarsonj@gmail.com>
2025-05-31 23:25:43 +0530

    Added Second Implementation
    Added Second Implementation

--- src/main/java/com/dsa/code/Arrays/SecondLargestElement.java
+++ src/main/java/com/dsa/code/Arrays/SecondLargestElement.java
@@ -18,8 +18,33 @@ class SecondLargestElement {
         return second;
     }
 
+    //Implementation - 2
+    // Worst case O(2n) = O(n) , because we used 2 loops here.
+    // Space Complexity = O(1) not extra spaces required.
+    public static int secondLargestElm(int[] nums){
+
+        int largest = 0;
+        int nextLargest = 0;
+
+        for(int i =0 ;i < nums.length ; i++){
+            if(largest<nums[i]){
+                largest = nums[i];
+            }
+        }
+
+        for(int j =0; j< nums.length; j++){
+            if(nextLargest<nums[j] && nums[j]!=largest){
+                nextLargest = nums[j];
+            }
+
+        }
+        return nextLargest;
+    }
+
     public static void main(String[] args) {
-        System.out.println(secondLargestElement(new int[]{10, 12, 10, 10}));
+      //  System.out.println(secondLargestElement(new int[]{10, 12, 10, 10}));
+
+        System.out.println(secondLargestElm(new int[]{10, 12, 11, 10}));
 
     }
 }
