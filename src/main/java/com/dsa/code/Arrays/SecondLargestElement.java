c0569d9a8e2715e667b0ba52883451c6b552080b
francisjebarsonj <francisjebarsonj@gmail.com>
2025-05-31 23:11:16 +0530

    Corrected Second Largest Element Problem

--- src/main/java/com/dsa/code/Arrays/SecondLargestElement.java
+++ src/main/java/com/dsa/code/Arrays/SecondLargestElement.java
@@ -1,22 +1,25 @@
 package com.dsa.code.Arrays;
 
+//Time Complexity: O(n), as we are traversing the array only once.
+//Auxiliary space: O(1)
 class SecondLargestElement {
     public static int secondLargestElement(int[] nums) {
         int first = 0;
         int second = 0;
-        for(int i:nums){
-            if(first<i){
-                first = i;
+        for(int i=0 ; i < nums.length; i++){
+            if(first<nums[i]){
+                second = first;
+                first = nums[i];
             }
-            else if (second < first && second < i){
-                second = i;
+            else if (second < nums[i] && first > nums[i] ){
+                second = nums[i];
             }
         }
         return second;
     }
 
     public static void main(String[] args) {
-        System.out.println(secondLargestElement(new int[]{3, 4, 10, 1}));
+        System.out.println(secondLargestElement(new int[]{10, 12, 10, 10}));
 
     }
 }
